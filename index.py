0f80af7195edaa67fb44b0fc822415a1d6d2d220
Matthew Brenneman <51242885+mdb5672@users.noreply.github.com>
2020-12-08 18:17:55 -0500

    Setup
    - Setup base code.

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 import os
 import stripe
 import json
+from flask import Flask, jsonify, request
 
 # Add stripe api key here (Use testing key until pushing live.)
 stripe.api_key = ''
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+
+# Parse the data
+def parse():
+    event = None
+    payload = request.data
+
+    try:
+        event = json.loads(payload)
+    except:
+        print(f'Error while parsing Webhook: {event}')
+    
