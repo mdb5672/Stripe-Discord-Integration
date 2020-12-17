8dfcfc79a59733de3cf4575cb31e04f6be7e2451
Matthew Brenneman <51242885+mdb5672@users.noreply.github.com>
2020-12-17 18:33:52 -0500

    Cleanup
    -Cleanup and remove unneeded files

--- index.py
+++ index.py
@@ -9,60 +9,75 @@ stripe.api_key = ''
 
 app = Flask(__name__)
 
-# Send discord webhook
+# Send notif
 def sendNotification(notification):
-    hook = Webhook(notification['webhook'])
-    embed = Embed(description=notification['description'], timestamp='now')
-    embed.set_title(name=notification['title'])
-    embed.set_footer(text=notification['footer'])
+    hook = Webhook('')
+    # Set embed info
+    embed = Embed(description=notification['description'],timestamp='now',color=notification['color'])
+    embed.set_footer(text='Stripe for Discord by mdb5672',
+                        icon_url='https://avatars1.githubusercontent.com/u/51242885?s=60&v=4')
     for fields in notification['fields']:
-        embed.add_field(name=fields['name'], value=fields['value'])
-
-    embed.set_footer(text=notification['footer'])
-
+        embed.add_field(name=str(fields['name']),value=str(fields['value']),inline=False)
     hook.send(embed=embed)
 
-@app.route('/webhook', methods=['POST'])
+#set listener url
+@app.route('/', methods=['POST'])
 
-#Stripe events to monitor:
- # charge.succeeded
- # charge.failed
- # payout.paid
+# Stripe events to monitor:
+    # charge.succeeded
+    # charge.failed
+    # invoice.marked_uncollectible
 
-
-# Parse the data
+# Handle incoming payload
 def parse():
     event = None
     payload = request.data
-    notification = {}
-    notification['fields'] = []
-    notification['webhook'] = ''
 
     try:
         event = json.loads(payload)
-    except:
-        print(f'Error while parsing Webhook: {event}')
+    except ValueError as e:
+        print('⚠️  Webhook error while parsing basic request.' + str(e))
+        return jsonify(success=True)
+    except stripe.error.SignatureVerificationError as e:
+        print('⚠️  Webhook error while parsing basic request.' + str(e))
         return jsonify(success=True)
 
     # Handle the event
-    if event and event['type'] == 'charge.succeeded':
-        payment_data = event['data']['object']  # contains a stripe.PaymentIntent
-        print('Payment for {} succeeded'.format(payment_data['amount']))
+    notification = {}
+    notification['fields'] = []
+    notification['color'] = []
+    notification['description'] = []
 
-        notification['title'] = 'New Payment'
-        notification['description'] = f'https://dashboard.stripe.com/payments/{payment_data['id']}'
-        notification['footer'] = 'Stripe to Discord'
-        notification['fields'].append({'name':'Email:', 'value':f'{payment_data['billing_details']['email']}'})
-        notification['fields'].append({'name':'User:', 'value':f'{payment_data['billing_details']['name']}'})
-        notification['fields'].append({'name':'Amount:', 'value':f'{payment_data['currency']} {payment_data['amount']/100}'})
+    if event['type'] == 'charge.succeeded':
+        paymentData = event['data']['object']
+        notification['description'] = 'Payment Successful!'
+        notification['fields'].append({'name': 'Customer', 'value': (paymentData['billing_details']['name'])})
+        notification['fields'].append({'name': 'Email:', 'value': (paymentData['billing_details']['email'])})
+        notification['fields'].append({'name': 'Amount:', 'value': (paymentData['currency']) + ' ' + str(paymentData['amount']/100)})
+        notification['color'] = '130817'
         sendNotification(notification)
 
-    elif event['type'] == 'payment_method.attached':
-        payment_data = event['data']['object']  # contains a stripe.PaymentMethod
-        # Then define and call a method to handle the successful attachment of a PaymentMethod.
-        # handle_payment_method_attached(payment_method)
-    else:
-        # Unexpected event type
-        print(f'Unhandled event type {event["type"]}')
+    elif event['type'] == 'charge.failed':
+        paymentData = event['data']['object']
+        notification['description'] = 'Payment Failed.'
+        notification['fields'].append({'name': 'Customer', 'value': paymentData['billing_details']['name']})
+        notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
+        notification['color'] = '16711680'
+        sendNotification(notification)
+
+    elif event['type'] == 'invoice.marked_uncollectible':
+        paymentData = event['data']['object']
+        notification['description'] = 'Invoice Unpaid.'
+        notification['fields'].append({'name': 'Customer', 'value': paymentData['customer_name']})
+        notification['fields'].append({'name': 'Email:', 'value': paymentData['customer_email']})
+        notification['fields'].append({'name': 'Amount Due:', 'value': paymentData['amount_due']/100})
+        notification['color'] = '16711680'
+        sendNotification(notification)
 
     return jsonify(success=True)
+
+
+if __name__ == '__main__':
+    # set listening port
+    app.run(port=8080)
