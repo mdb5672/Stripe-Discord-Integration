cb49f343b05374fbb59e8d42ec27432726b39237
Matthew Brenneman <51242885+mdb5672@users.noreply.github.com>
2020-12-17 18:30:01 -0500

    Remove test material
    -test material removed

--- new.py
+++ new.py
@@ -5,29 +5,28 @@ from flask import Flask, jsonify, request
 from dhooks import Webhook, Embed
 
 # Add stripe api key here (Use testing key until pushing live.)
-stripe.api_key = 'pk_test_51HufjkKd0aYbOLmutLvouIrBrKoCUAGmtQUa38yKMuZMSmLwWB3pXLlB6mtiuqrXvvlwdBndaFSEHeRflIMZQmub00LIU9w2Ls'
+stripe.api_key = ''
 
 app = Flask(__name__)
 
 # Send notif
 def sendNotification(notification):
-    hook = Webhook('https://canary.discord.com/api/webhooks/788922218314203167/YridU4VvmzYzgaJk-7JlLGALSmpnTGtfMLhRe5wqnejW_jYLq5-6QRv8IUm3IQ_CI0fS')
+    print(notification)
+    hook = Webhook('')
     # Set embed info
-    # embed = Embed(description=notification['description'],timestamp='now',color=notification['color'],name='Stripe',avatar='https://pbs.twimg.com/profile_images/1280236709825835008/HmeYTwai_400x400.png')
-    embed = Embed()
+    embed = Embed(description=notification['description'],timestamp='now',color=notification['color'])
     embed.set_footer(text='Stripe for Discord by mdb5672',
                         icon_url='https://avatars1.githubusercontent.com/u/51242885?s=60&v=4')
     for fields in notification['fields']:
-        embed.add_field(name=fields['name'],value=fields['value'])
+        print(str(fields['name']) + str(fields['value']))
+        embed.add_field(name=str(fields['name']),value=str(fields['value']),inline=False)
     hook.send(embed=embed)
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 
 # Stripe events to monitor:
     # charge.succeeded
     # charge.failed
-    # customer.subscription.updated
-    # customer.subscription.deleted
     # invoice.marked_uncollectible
 
 # Handle incoming payload
@@ -47,14 +46,17 @@ def parse():
     # Handle the event
     notification = {}
     notification['fields'] = []
+    notification['color'] = []
+    notification['description'] = []
 
     if event['type'] == 'charge.succeeded':
         paymentData = event['data']['object']
+        print(paymentData)
         notification['description'] = 'Payment Successful!'
-        notification['fields'].append({'name': 'Customer', 'value': paymentData['billing_details']['name']})
-        notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
-        notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
-        notification['color'] = '01FF01'
+        notification['fields'].append({'name': 'Customer', 'value': (paymentData['billing_details']['name'])})
+        notification['fields'].append({'name': 'Email:', 'value': (paymentData['billing_details']['email'])})
+        notification['fields'].append({'name': 'Amount:', 'value': (paymentData['currency']) + ' ' + str(paymentData['amount']/100)})
+        notification['color'] = '130817'
         sendNotification(notification)
 
     elif event['type'] == 'charge.failed':
@@ -63,15 +65,7 @@ def parse():
         notification['fields'].append({'name': 'Customer', 'value': paymentData['billing_details']['name']})
         notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
         notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
-        notification['color'] = 'FF0000'
-        sendNotification(notification)
-    
-    elif event['type'] == 'customer.subscription.updated':
-        paymentData = event['data']['object']
-        notification['description'] = 'Subscription Updated.'
-        notification['fields'].append({'name': 'Customer ID', 'value': paymentData['customer']})
-        notification['fields'].append({'name': 'New Tier:', 'value': paymentData['items']['data']['price']['product']})
-        notification['color'] = 'FFFF00'
+        notification['color'] = '16711680'
         sendNotification(notification)
 
     elif event['type'] == 'invoice.marked_uncollectible':
@@ -80,14 +74,7 @@ def parse():
         notification['fields'].append({'name': 'Customer', 'value': paymentData['customer_name']})
         notification['fields'].append({'name': 'Email:', 'value': paymentData['customer_email']})
         notification['fields'].append({'name': 'Amount Due:', 'value': paymentData['amount_due']/100})
-        notification['color'] = 'FF0000'
-        sendNotification(notification)
-
-    elif event['type'] == 'customer.subscription.deleted':
-        paymentData = event['data']['object']
-        notification['description'] = 'Invoice Unpaid.'
-        notification['fields'].append({'name': 'Customer ID', 'value': paymentData['customer']})
-        notification['color'] = 'FF0000'
+        notification['color'] = '16711680'
         sendNotification(notification)
 
     return jsonify(success=True)
