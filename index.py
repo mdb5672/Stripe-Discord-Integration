c5b48ca3e093c10dbd9f77b4461587e321588df2
Matthew Brenneman <51242885+mdb5672@users.noreply.github.com>
2021-01-16 15:34:03 -0500

    Add events
    - add refund, subscriptions, disputes and payouts
    - remove uncollectable
    - add endpoint secret

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ from dhooks import Webhook, Embed
 
 # Add stripe api key here (Use testing key until pushing live.)
 stripe.api_key = ''
+# Add stripe secret key here
+endpoint_secret = ''
 
 app = Flask(__name__)
 
@@ -26,7 +28,16 @@ def sendNotification(notification):
 # Stripe events to monitor:
     # charge.succeeded
     # charge.failed
-    # invoice.marked_uncollectible
+    # charge.refunded
+    # invoice.paid
+    # customer.subscription.created
+    # customer.subscription.deleted
+    # customer.subscription.updated
+    # customer.subscription.trail_will_end
+    # charge.dispute.created
+    # charge.dispute.closed
+    # payout.paid
+    # payout.failed
 
 # Handle incoming payload
 def parse():
@@ -41,6 +52,18 @@ def parse():
     except stripe.error.SignatureVerificationError as e:
         print('⚠️  Webhook error while parsing basic request.' + str(e))
         return jsonify(success=True)
+    
+    if endpoint_secret:
+        # Only verify the event if there is an endpoint secret defined
+        # Otherwise use the basic event deserialized with json
+        sig_header = request.headers.get('stripe-signature')
+        try:
+            event = stripe.Webhook.construct_event(
+                payload, sig_header, endpoint_secret
+            )
+        except stripe.error.SignatureVerificationError as e:
+            print('⚠️  Webhook signature verification failed.' + str(e))
+            return jsonify(success=True)
 
     # Handle the event
     notification = {}
@@ -50,34 +73,122 @@ def parse():
 
     if event['type'] == 'charge.succeeded':
         paymentData = event['data']['object']
-        notification['description'] = 'Payment Successful!'
-        notification['fields'].append({'name': 'Customer', 'value': (paymentData['billing_details']['name'])})
-        notification['fields'].append({'name': 'Email:', 'value': (paymentData['billing_details']['email'])})
-        notification['fields'].append({'name': 'Amount:', 'value': (paymentData['currency']) + ' ' + str(paymentData['amount']/100)})
+        notification['description'] = 'Payment Successful'
+        notification['fields'].append({'name': 'Customer:', 'value': paymentData['billing_details']['name']})
+        notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
         notification['color'] = '130817'
         sendNotification(notification)
 
     elif event['type'] == 'charge.failed':
         paymentData = event['data']['object']
-        notification['description'] = 'Payment Failed.'
-        notification['fields'].append({'name': 'Customer', 'value': paymentData['billing_details']['name']})
+        notification['description'] = 'Payment Failed'
+        notification['fields'].append({'name': 'Customer:', 'value': paymentData['billing_details']['name']})
         notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
         notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
         notification['color'] = '16711680'
         sendNotification(notification)
 
-    elif event['type'] == 'invoice.marked_uncollectible':
+    elif event['type'] == 'charge.refunded':
         paymentData = event['data']['object']
-        notification['description'] = 'Invoice Unpaid.'
-        notification['fields'].append({'name': 'Customer', 'value': paymentData['customer_name']})
+        notification['description'] = 'Payment refunded'
+        notification['fields'].append({'name': 'Customer:', 'value': paymentData['billing_details']['name']})
+        notification['fields'].append({'name': 'Email:', 'value': paymentData['billing_details']['email']})
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Amount Refunded:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount_refunded']/100)})
+        notification['color'] = '16711680'
+        sendNotification(notification)
+
+    elif event['type'] == 'invoice.paid':
+        paymentData = event['data']['object']
+        notification['description'] = 'Invoice Paid'
+        notification['fields'].append({'name': 'Customer:', 'value': paymentData['customer_name']})
         notification['fields'].append({'name': 'Email:', 'value': paymentData['customer_email']})
         notification['fields'].append({'name': 'Amount Due:', 'value': paymentData['amount_due']/100})
+        notification['color'] = '130817'
+        sendNotification(notification)
+    
+    elif event['type'] == 'customer.subscription.created':
+        paymentData = event['data']['object']
+        notification['description'] = 'New Subscription'
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Subscription Tier:', 'value': paymentData['items']['data']['price']['nickname']})
+        notification['fields'].append({'name': 'Price:', 'value': paymentData['currency'] + ' ' + str(paymentData['items']['data']['price']['unit_amount']/100)})
+        notification['fields'].append({'name': 'Discount:', 'value': paymentData['discount']})
+        notification['color'] = '130817'
+        sendNotification(notification)
+
+    elif event['type'] == 'customer.subscription.deleted':
+        paymentData = event['data']['object']
+        notification['description'] = 'Subscription Canceled'
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Subscription Tier:', 'value': paymentData['items']['data']['price']['nickname']})
         notification['color'] = '16711680'
+        sendNotification(notification)
+    
+    elif event['type'] == 'customer.subscription.updated':
+        paymentData = event['data']['object']
+        notification['description'] = 'Subscription Updated'
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Subscription Tier:', 'value': paymentData['items']['data']['price']['nickname']})
+        notification['fields'].append({'name': 'Price:', 'value': paymentData['currency'] + ' ' + str(paymentData['items']['data']['price']['unit_amount']/100)})
+        notification['fields'].append({'name': 'Discount:', 'value': paymentData['discount']})
+        notification['color'] = '9896172'
+        sendNotification(notification)
+
+    elif event['type'] == 'customer.subscription.trial_will_end':
+        paymentData = event['data']['object']
+        notification['description'] = 'Trial Ending'
+        notification['fields'].append({'name': 'Customer ID:', 'value': paymentData['customer']})
+        notification['fields'].append({'name': 'Subscription Tier:', 'value': paymentData['items']['data']['price']['nickname']})
+        notification['fields'].append({'name': 'Trial End Date:', 'value': paymentData['trial_end']})
+        notification['fields'].append({'name': 'Price:', 'value': paymentData['currency'] + ' ' + str(paymentData['items']['data']['price']['unit_amount']/100)})
+        notification['fields'].append({'name': 'Discount:', 'value': paymentData['discount']})
+        notification['color'] = '9896172'
+        sendNotification(notification)
+
+    elif event['type'] == 'charge.dispute.created':
+        paymentData = event['data']['object']
+        notification['description'] = 'Charge Disputed'
+        notification['fields'].append({'name': 'Charge ID:', 'value': paymentData['charge']})
+        notification['fields'].append({'name': 'Reason:', 'value': paymentData['reason']})
+        notification['fields'].append({'name': 'Status:', 'value': paymentData['status']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
+        notification['color'] = '16711680'
+        sendNotification(notification)
+    
+    elif event['type'] == 'charge.dispute.closed':
+        paymentData = event['data']['object']
+        notification['description'] = 'Dispute Closed'
+        notification['fields'].append({'name': 'Charge ID:', 'value': paymentData['charge']})
+        notification['fields'].append({'name': 'Reason:', 'value': paymentData['reason']})
+        notification['fields'].append({'name': 'Status:', 'value': paymentData['status']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['currency'] + ' ' + str(paymentData['amount']/100)})
+        notification['color'] = '16607746'
+        sendNotification(notification)
+    
+    elif event['type'] == 'payout.paid':
+        paymentData = event['data']['object']
+        notification['description'] = 'Payout Sent'
+        notification['fields'].append({'name': 'Payout ID:', 'value': paymentData['id']})
+        notification['fields'].append({'name': 'Status:', 'value': paymentData['status']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['amount']})
+        notification['color'] = '130817'
+        sendNotification(notification)
+    
+    elif event['type'] == 'payout.failed':
+        paymentData = event['data']['object']
+        notification['description'] = 'Payout Failed'
+        notification['fields'].append({'name': 'Payout ID:', 'value': paymentData['id']})
+        notification['fields'].append({'name': 'Status:', 'value': paymentData['status']})
+        notification['fields'].append({'name': 'Amount:', 'value': paymentData['amount']})
+        notification['color'] = '16607746'
         sendNotification(notification)
 
     return jsonify(success=True)
 
 
 if __name__ == '__main__':
-    # set listening port
-    app.run(port=8080)
+    app.run()
